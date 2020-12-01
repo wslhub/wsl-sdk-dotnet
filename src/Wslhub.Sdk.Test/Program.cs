4280419639cc1de91a809d22cfa895bd8aeeba03
Jung Hyun Nam <rkttu@rkttu.com>
2020-12-02 01:04:52 +0900

    Change unit test method - MSTest and Visual Studio does not support CoInitializeEx - Without CoInitializeEx, WSL APIs are not working.

---
+++ src/Wslhub.Sdk.Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using Bullseye;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Wslhub.Sdk.Test
+{
+    internal static partial class Program
+    {
+        private static void Main()
+        {
+            Wsl.InitializeSecurityModel();
+
+            var methods = typeof(WslTest)
+                .GetMethods(BindingFlags.Public | BindingFlags.NonPublic| BindingFlags.Static)
+                .Where(x => x.Name.StartsWith("Test_", StringComparison.Ordinal))
+                .OrderBy(x => Guid.NewGuid())
+                .ToArray();
+
+            var targets = new Targets();
+            var methodNames = new List<string>(methods.Length);
+
+            foreach (var eachMethod in methods)
+            {
+                methodNames.Add(eachMethod.Name);
+                targets.Add(eachMethod.Name, () => eachMethod.Invoke(null, null));
+            }
+
+            targets.RunAndExit(methodNames);
+        }
+    }
+}
--- src/Wslhub.Sdk.Test/WslTest.cs
+++ src/Wslhub.Sdk.Test/WslTest.cs
@@ -1,29 +1,49 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿#pragma warning disable IDE0051 // Remove unused private members
+
+using System;
 using System.Linq;
 
 namespace Wslhub.Sdk.Test
 {
-    [TestClass]
-    public class WslTest
+    static class WslTest
     {
-        [TestMethod]
-        public void SystemAssertion()
+        static void Test_SystemAssertion()
         {
             Wsl.AssertWslSupported();
         }
 
-        [TestMethod]
-        public void GetDistroListFromRegistryTest()
+        static void Test_GetDistroListFromRegistryTest()
         {
             var distros = Wsl.GetDistroListFromRegistry();
-            Assert.IsNotNull(distros);
+
+            if (distros == null)
+                throw new ArgumentNullException(nameof(distros));
         }
 
-        [TestMethod]
-        public void QueryDistroInfo()
+        static void Test_QueryDistroInfo()
         {
             var distros = Wsl.GetDistroQueryResult();
-            Assert.IsNotNull(distros);
+
+            if (distros == null)
+                throw new ArgumentNullException(nameof(distros));
+
+            if (distros.Count(x => x.IsRegistered == false) > 0)
+                throw new Exception("Cannot query distro properties.");
+        }
+
+        static void Test_ExecTest()
+        {
+            var distroName = Wsl.GetDistroListFromRegistry().Select(x => x.DistroName).First();
+
+            var result = Wsl.RunWslCommand(distroName, "cat /etc/passwd");
+
+            if (result == null)
+                throw new ArgumentNullException(nameof(result));
+
+            if (result.Length == 0)
+                throw new Exception("No output.");
         }
     }
 }
+
+#pragma warning restore IDE0051 // Remove unused private members
